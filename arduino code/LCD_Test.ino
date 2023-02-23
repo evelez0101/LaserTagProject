ef064357c2f6387a4299610451fa97bcd92569bd
evelez0101 <114781973+evelez0101@users.noreply.github.com>
2023-02-23 12:34:31 -0500

    Update LCD_Test.ino

--- arduino code/LCD_Test.ino
+++ arduino code/LCD_Test.ino
@@ -3,43 +3,28 @@
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
+String top = "Ammunition: ";
+int ammo = 90;
+String bottom = "Health: "; 
+String health = "######"; 
 
-int AMMO = 90;
-String t = "Ammunition: " + (String)AMMO;
-String r = "#### Reload ####";
-String b = "Health: ####### ";
 
 void setup() {
-  // Set up 16 by 2 character matrix 
+  // Initialozes LCD
   lcd.begin(16, 2);
-  // you can now interact with the LCD, e.g.:
-  screen();
 }
 
-void loop() {
-
-}
-
-void screen() // Need to implment dynamic screen fucntion later
+void loop() 
 {
-  // Sets cursor to print top row 
+  displayScreen();
+
+}
+
+// Displays Ammo on top and Health on bottom
+void displayScreen()
+{
   lcd.setCursor(0,0);
-  lcd.print(t);
-
-  if (AMMO == 0)
-  {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print(r);
-  }
-
-  // Sets cursor to print bottom row
-  lcd.setCursor(0, 1);
-  lcd.print(b);
-}
-
-void shoot()
-{
-  AMMO--;
-  t = "Ammunition: " + (String)AMMO;
+  lcd.print(top + ammo);
+  lcd.setCursor(0,1);
+  lcd.print(bottom + health);
 }
